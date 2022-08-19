4237072f62ea2f8ed34b86d7cc9ba964bab7ad9c
dobby <tlswotjd33@naver.com>
2022-08-19 17:59:41 +0900

    lg공익재단

---
+++ 0818(목) - LG공익재단/js/main.js
@@ -0,0 +1,11 @@
+$(document).redy(function(){
+
+    let scrolling - $(window).scrolltop()
+    $(window).scroll(function(){
+        scrolling = $(window).scrolltop()
+        console.log(scrolling)
+        if(scrolling > 0)(
+            $
+        )
+    });
+});
